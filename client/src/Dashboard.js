888094df8af1a33acf3d1d725e1e7416e755ade7
unknown <kevinma91@yahoo.com>
2019-04-12 17:20:10 -0700

    added fake central state for authentication

---
+++ client/src/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1> Hello </h1>
+      </div>
+    )
+  }
+}
+
+export default Dashboard;
